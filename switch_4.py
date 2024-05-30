b3e1acc3825dfb50d3772a172df2cd6634cf3eed
succes <succes@MacBook-Pro-succes.local>
2024-05-30 22:41:31 +0400

    Выполнено задание 4

--- switch_4.py
+++ switch_4.py
@@ -4,3 +4,50 @@
 # Вывести направление робота после выполнения полученной команды.
 
 
+def check_action(turn_key: str, action: int):
+    """Функция вызывает необходимое действие с направлением в зависимости от действия
+
+    Args:
+        turn_key (str): Текущее направление
+        action (int): Действие для изменения направления
+
+    Returns:
+        _type_: str
+    """
+    match action:
+        case 0:
+            return turn_key
+        case 1:
+            return change_turn_left(turn_key=turn_key)
+        case -1:
+            return change_turn_right(turn_key=turn_key)
+            
+
+def change_turn_left(turn_key: str):
+    new_turn = {
+        'С': 'З',
+        'З': 'Ю',
+        'Ю': 'В',
+        'В': 'С'
+    }
+    return new_turn.get(turn_key, "Такого направления не существует")
+      
+      
+def change_turn_right(turn_key: str):
+    new_turn = {
+        'С': 'В',
+        'В': 'Ю',
+        'Ю': 'З',
+        'З': 'С'
+    }
+    return new_turn.get(turn_key, "Такого направления не существует")
+
+
+turn_key = input()
+try:
+    action = int(input())
+except ValueError:
+    print('Неправильное действие, выберите 0, 1 или -1')
+
+
+print(check_action(turn_key=turn_key, action=action))
